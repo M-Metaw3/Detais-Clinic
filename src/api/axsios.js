1a6fb2a2063763da768ab503b23a166f58d85e1e
M-Metaw3 <mizoomizoo161@gmail.com>
2024-02-11 11:25:23 +0200

    change public

--- src/api/axsios.js
+++ src/api/axsios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 // Server URL
- const axiosBaseUrl = axios.create({baseURL: "http://localhost:3111"});
+//  const axiosBaseUrl = axios.create({baseURL: "http://localhost:3111"});
+ const axiosBaseUrl = axios.create({baseURL: "https://tough-ox-hospital-gown.cyclic.app"});
+
 // const axiosBaseUrl = axios.create({baseURL: "https://e-commerce-9kwf.onrender.com/api"});
 
 
