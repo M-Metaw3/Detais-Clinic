30b2516c6a7255bb136b7675dc7f476cd5423785
M-Metaw3 <mizoomizoo161@gmail.com>
2024-03-21 03:55:04 +0200

    new api

--- src/api/ApiUrl.js
+++ src/api/ApiUrl.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 // Server URL
- const axiosBaseUrl = axios.create({baseURL: "http://localhost:3111"});
-//  const Url = "https://deteeth-clinic.onrender.com"
-const Url = "http://localhost:3111"
+//  const axiosBaseUrl = axios.create({baseURL: "http://localhost:3111"});
+ const Url = "http://143.110.153.206"
+// const Url = "http://localhost:3111"
 
 
 // const axiosBaseUrl = axios.create({baseURL: "https://e-commerce-9kwf.onrender.com/api"});
